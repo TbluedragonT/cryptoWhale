49ac2e46425e1a7b3e9f948816d9c6ace4ab49c2
TbluedragonT <bluedragon319331@gmail.com>
2021-10-04 19:58:21 -0400

    blank out

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
 // Core React
 import Layout from "@components/layout/Layout/Layout"
-import Homepage from "@components/partials/HomePage"
 import React from "react"
 
 // Render
 const WalletWinners = () => (
   <Layout
-    title="WalletWinners"
-    contentClassName="homePageContent px-0 max-w-full bg-dark-whelps"
+    title="Crypto Whale Club"
+    contentClassName="homePageContent px-0 max-w-full"
   >
-    <Homepage />
+    <div className="text-4xl text-primary text-center mt-32 font-bold">Coming Soon.</div>
   </Layout>
 )
 export default WalletWinners
