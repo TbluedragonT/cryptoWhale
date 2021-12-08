6211d66a05ee4239fce78fffd898ee47aa1d032c
TbluedragonT <bluedragon319331@gmail.com>
2021-12-08 16:17:52 -0600

    add mint page

---
+++ src/config/address.js
@@ -0,0 +1,5 @@
+export default {
+    cwc_contract: {
+        4: '0xB30014fC58c9A1E867270968f3bCD4A526400811' // Rinked TestNet
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  mode: 'jit',
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],  // TODO: enable in production
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"], // TODO: enable in production
   darkMode: false, // or 'media' or 'class'
   theme: {
     boxShadow: {
-      'lg': '0 4px 6px -2px rgba(0, 0, 0, 1)',
+      lg: "0 4px 6px -2px rgba(0, 0, 0, 1)",
     },
     fontFamily: {
       sans: ["Barlow", "sans-serif"],
@@ -17,6 +17,15 @@ module.exports = {
         xl: "8rem",
         "2xl": "3rem",
       },
+    },
+    screens: {
+      tiny: "400px",
+      xs: "541px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     extend: {
       backgroundImage: {
@@ -33,7 +42,7 @@ module.exports = {
         },
       },
       ringWidth: {
-        '16': '16px',
+        16: "16px",
       },
       width: {
         "1/16": "6.25%",
@@ -47,12 +56,18 @@ module.exports = {
         "2/16": "12.5%",
         "3/16": "18.75%",
       },
+      spacing: {
+        30:"30px",
+        normal: "700px",
+        "h-normal": "400px",
+        "h-small": "250px",
+        "w-tiny": "250px",
+        "h-tiny": "200px",
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [require("@tailwindcss/line-clamp")],
 }
