5727ba7e3c7e1808f76a5c0a21ba5e8f2ecb40c7
TbluedragonT <bluedragon319331@gmail.com>
2021-12-16 05:21:34 +0200

    Implement virtual staking

--- src/components/layout/Layout/Layout.js
+++ src/components/layout/Layout/Layout.js
@@ -126,6 +126,7 @@ const Layout = ({
         return;
       }
       setConnected(await onBoard.walletCheck())
+      console.log(await onBoard.walletCheck())
     }
   }
 
--- src/config/address.js
+++ src/config/address.js
@@ -2,5 +2,9 @@ export default {
     cwc_contract: {
         1: '0x0fF6FF088A3dc5F56cF9eDFA411AcFdfFFc59735', // Ethereum Mainnet
         4: '0x236a0D6B2DE262567b280B602B19CE6aC1493c4a' // Rinked TestNet
+    },
+    blub_contract: {
+        1: '0xBCC02884f011e8d420C74171F51B8224f18005F3', // Ethereum Mainnet
+        4: '0xBCC02884f011e8d420C74171F51B8224f18005F3', // Rinkeby TestNet
     }
 }
