f357e40f45263945430d52bf0f9fda17cb0f866e
TbluedragonT <bluedragon319331@gmail.com>
2021-12-15 22:58:20 -0500

    updated real blub contract

--- src/config/address.js
+++ src/config/address.js
@@ -4,7 +4,7 @@ export default {
         4: '0x236a0D6B2DE262567b280B602B19CE6aC1493c4a' // Rinked TestNet
     },
     blub_contract: {
-        1: '0xBCC02884f011e8d420C74171F51B8224f18005F3', // Ethereum Mainnet
+        1: '0x1603C89571fCeec56a32Da30433890a065540B0d', // Ethereum Mainnet
         4: '0xBCC02884f011e8d420C74171F51B8224f18005F3', // Rinkeby TestNet
     }
 }
